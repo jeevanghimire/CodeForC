8a15fc870154829f5397fc691d8ce1f4759f0947
jeevanghimire <imjeevanghimire@gmail.com>
2023-04-27 22:21:42 +0545

    New files

---
+++ ifelse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    if (a > b)
+    {
+        printf("a is greater than b\n");
+    }
+    else
+    {
+        printf("a is not greater than b\n");
+    }
+
+    return 0;
+}
