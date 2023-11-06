8a4f9d09670b978b53267631efec7008f8d95dd8
Jeevan Ghimire <imjeevanghimire@gmail.com>
2023-11-06 23:13:39 +0545

    nice job is done

---
+++ function_in_C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// void PatternPrint(numb)
+// {
+
+//     int i, j;
+//     for (i = 0; i < numb1; i++)
+//     {
+//         for (j = i; j < numb1; j++)
+//         {
+//             printf(" * ");
+//         }
+//         printf("\n");
+//     }
+// }
+
+int SumOfNumber(numb1, numb2)
+{
+    return numb1 + numb2;
+}
+
+int main()
+{
+
+    printf("the Result is = %d\n", SumOfNumber(23, 78));
+    return 0;
+}
+
