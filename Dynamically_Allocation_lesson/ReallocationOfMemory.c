4414f0d42fae7cef8e34e7b5326b55ea605e6294
Jeevan Ghimire <imjeevanghimire@gmail.com>
2023-11-11 22:40:47 +0545

    some changes

--- Dynamically_Allocation_lesson/ReallocationOfMemory.c
+++ Dynamically_Allocation_lesson/ReallocationOfMemory.c
@@ -13,6 +13,7 @@ void print_loop(int *array, int n)
 int main()
 {
     int numb, i, j, AddedNumber;
+    char count;
     printf("Enter the number of interger:\n");
     scanf("%d", &numb);
     int *number = (int *)malloc(numb * sizeof(int));
@@ -30,7 +31,6 @@ int main()
     print_loop(number, numb);
 
     printf("Do you want to add more number :\n");
-    
     scanf("%d", &AddedNumber);
     int *temp = realloc(number, (numb + AddedNumber) * sizeof(int));
     if (temp == NULL)
@@ -43,10 +43,13 @@ int main()
     {
         number = temp;
     }
-    for (i = 0; i < (numb+AddedNumber); i++)
+    for (i = 0; i < (numb + AddedNumber); i++)
     {
         scanf("%d", &number[i]);
     }
+
+    printf("Without Reallocation of memeory:\n");
+    print_loop(number, numb);
 
     printf("After Allocation of Memory:\n");
     print_loop(number, numb + AddedNumber);
